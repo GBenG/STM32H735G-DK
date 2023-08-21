daaf84e02b00e99e833c82c29f324a8ad608b249
Ben.G <sps.tech.way@gmail.com>
2023-08-21 20:57:12 +0300

    Add HW led control

--- TouchGFX/TGFX_led_control/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
+++ TouchGFX/TGFX_led_control/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -15,3 +15,17 @@ void Screen1View::tearDownScreen()
 {
     Screen1ViewBase::tearDownScreen();
 }
+
+
+//Toggle LED function
+//PC2 RED LED
+//PC3 GRN LED
+void Screen1View::ToggleLED(){
+	if( toggleButton.getState() == true ){
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_RESET);
+	}else{
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_3, GPIO_PIN_RESET);
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_2, GPIO_PIN_SET);
+	}
+}
