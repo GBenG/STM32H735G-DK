1cd154c751380253f606120093086f81e9e8d0cc
Ben.G <sps.tech.way@gmail.com>
2023-08-21 20:56:45 +0300

    GUI Toggle

---
+++ TouchGFX/TGFX_led_control/TouchGFX/gui/src/screen1_screen/Screen1View.cpp
@@ -0,0 +1,17 @@
+#include <gui/screen1_screen/Screen1View.hpp>
+#include "stm32h7xx_hal.h"
+
+Screen1View::Screen1View()
+{
+
+}
+
+void Screen1View::setupScreen()
+{
+    Screen1ViewBase::setupScreen();
+}
+
+void Screen1View::tearDownScreen()
+{
+    Screen1ViewBase::tearDownScreen();
+}
