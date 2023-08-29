1ee8eab0b7be376c9a2d90f5a68b8f64ef8bec4f
Ben.G <sps.tech.way@gmail.com>
2023-08-29 23:40:41 +0300

    add some bouncing algorithm

--- TouchGFX/Eye_Container/TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
+++ TouchGFX/Eye_Container/TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
@@ -1,6 +1,19 @@
 #include <gui/mainscreen_screen/MainScreenView.hpp>
+#include <touchgfx/hal/OSWrappers.hpp>
 #include <stdint.h>
 #include <stdlib.h>
+
+
+//local data
+static 	uint16_t _x_vect = 3;
+static	uint16_t _y_vect = 1;
+static	uint16_t _x = 287;
+static	uint16_t _y = 30;
+
+//Display borders size
+#define DISP_WIDTH 480
+#define DISP_HIGHT 272
+
 
 MainScreenView::MainScreenView()
 {
@@ -18,37 +31,33 @@ void MainScreenView::tearDownScreen()
 }
 
 void MainScreenView::ScreenButtonClicked(){
-
+	//Generate new direction
+	if( _y < DISP_HIGHT-Eye_R.getHeight() && _y > 0 ){
+		_y_vect = rand() % 11;
+	}
+	if( _x < DISP_WIDTH-Eye_R.getWidth() && _x > 0 ){
+		_x_vect = rand() % 11;
+	}
 }
 
+
 void MainScreenView::ScreenTick(){
-	static 	uint16_t _x_dest = 339;
-	static	uint16_t _y_dest = 133;
-			uint16_t _x = Eye_R.getX();
-			uint16_t _y = Eye_R.getY();
+	//Calculate movement
+	_x=_x+_x_vect;
+	_y=_y+_y_vect;
 
-	//X
-	if( _x != _x_dest ){
-		if( _x < _x_dest ){
-			_x++;
-		}else{
-			_x--;
-		}
-	}else{
-		_x_dest = rand() % 339;
+	//Calculate edge bounce
+	if( _y >= DISP_HIGHT-Eye_R.getHeight() || _y <= 0 ){
+		_y_vect =- _y_vect;
 	}
-	//Y
-	if( _y != _y_dest ){
-		if( _y < _y_dest ){
-			_y++;
-		}else{
-			_y--;
-		}
-	}else{
-		_y_dest = rand() % 133;
+	if( _x >= DISP_WIDTH-Eye_R.getWidth() || _x <= 0 ){
+		_x_vect =- _x_vect;
 	}
 
+	//Draw
 	Eye_R.invalidate();
 	Eye_R.setXY(_x, _y);
 	Eye_R.invalidate();
+
+	OSWrappers::taskDelay(10);
 }
